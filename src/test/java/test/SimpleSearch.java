3ec51916f2bbc3d73da35018c87b08614c7f2b88
SSDmitriy <SomeMail@mail.ru>
2021-05-21 15:51:03 +0400

    add @beforeAll, @beforeEach three tests in one

--- src/test/java/test/SimpleSearch.java
+++ src/test/java/test/SimpleSearch.java
@@ -1,28 +1,70 @@
 package test;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.*;
 
 public class SimpleSearch {
-    //public static void main(String[] args) {
-        //System.out.println("Hello world!");
 
-        @Test
-        void wikipediaSearchTest() {
+    /**
+     * @BeforAll
+     * @BeforeEach
+     * @Test
+     * @AfterEach
+     * @BeforeEach
+     * @Test
+     * @AfterEach
+     * @AfterAll
+     */
 
-            //open google
-            open("https://google.com");
+    static Logger logger = LoggerFactory.getLogger(SimpleSearch.class);
 
-            //type search "wikipedia.org", press Enter
-            $("[name=q]").setValue("wikipedia").pressEnter();
+    @BeforeAll
+    static void setUpConfig() {
+        logger.info("@BeforeAll");
+        Configuration.browser = "chrome";
+        Configuration.startMaximized = true;
+    }
 
-            //check "Википедия — свободная энциклопедия" in result
-            $("#rso").shouldHave(text("wikipedia.org"));
-        }
-    //}
+    @BeforeEach
+    void openGooglePage() {
+        logger.info("@BeforeEach");
+        open("https://www.google.com");
+    }
+
+    @AfterEach
+    void closeBrowser() {
+        logger.info("@AfterEach");
+        closeWebDriver();
+    }
+
+    @Test
+    void wikipediaSearchTest() {
+        $(byName("q")).setValue("Wikipedia").pressEnter();
+        $("#search").shouldHave(text("Wikipedia.org"));
+    }
+
+    @Test
+    void selenideSearchTest() {
+        $(byName("q")).setValue("Selenide").pressEnter();
+        $("#search").shouldHave(text("selenide.org"));
+    }
+
+    @Test
+    void allureSearchTest() {
+        $(byName("q")).setValue("Allure testops").pressEnter();
+        $("#search").shouldHave(text("qameta.io"));
+    }
+
 }
 
 
